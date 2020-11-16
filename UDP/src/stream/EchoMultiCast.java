9d880a3fd1d82281e4d1a9fe1f07f95168118dee
INSArthur <49277717+INSArthur@users.noreply.github.com>
2020-11-16 15:52:26 +0100

    UDP chat

---
+++ UDP/src/stream/EchoMultiCast.java
@@ -0,0 +1,94 @@
+/***
+ * EchoServer
+ * Example of a TCP server
+ * Date: 10/01/04
+ * Authors:
+ */
+
+
+import java.io.*;
+import java.net.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class EchoMultiCast  {
+  
+ 	/**
+  	* main method
+	* @param EchoServer port
+  	* 
+  	**/
+
+    public static void main(String args[]){ 
+
+      if (args.length != 2) {
+        System.out.println("Usage: java EchoMultiCast <MultiCast adress> <MultiCast port>");
+        System.exit(1);
+      }
+
+      InetAddress groupAddress = null;
+      int groupPort = 0;
+      MulticastSocket multiCastSocket = null;
+
+      try{
+        //Group id address
+        groupAddress = InetAddress.getByName(args[0]);
+        groupPort = Integer.parseInt(args[1]);
+      }catch (Exception e){
+        System.err.println("Error in EchoMultiCast:" + e);    
+      }
+
+      try{
+        //Create a multicast socket
+        multiCastSocket = new MulticastSocket(groupPort);
+      }catch (IOException e){
+        System.err.println("Error in EchoMultiCast, multicast instanciation:" + e);   
+      }
+
+      //Join the group
+      try{
+        multiCastSocket.joinGroup(groupAddress);
+      }catch(IOException e){
+        System.err.println("Error in EchoMultiCast, multicast join:" + e);   
+      }
+
+      //New thread to display messages
+      EchoMultiCastDisplay md = new EchoMultiCastDisplay(multiCastSocket);
+      md.start();
+
+      //Canaux de récupération des messages
+      BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+      //Récupération des messages et envoi
+      String line;
+      while (true) {
+        try{
+          line = stdIn.readLine();
+          if (line.equals(".")) break;
+          if(!line.isEmpty()){
+            DatagramPacket msg = new DatagramPacket(line.getBytes(), line.length(), groupAddress, groupPort);
+            multiCastSocket.send(msg);
+          }
+        }catch (IOException e){
+          System.err.println("Error in EchoMultiCast, unable to acquire or sent the message:" + e);
+        }
+      }
+            
+      /*
+    	try {
+    		System.out.println("Server ready..."); 
+    		while (true) {
+    		}
+      } catch (Exception e) {
+          System.err.println("Error in EchoServer:" + e);
+      }
+      */
+      try{
+        multiCastSocket.leaveGroup(groupAddress);
+      }catch (IOException e){
+        System.err.println("Error in EchoMultiCast, unable to leave the group:" + e);
+      }
+    }
+}
+
+  
---
+++ UDP/src/stream/EchoMulticastDisplay.java
@@ -0,0 +1,44 @@
+/***
+ * EchoClient
+ * Example of a TCP client 
+ * Date: 10/01/04
+ * Authors:
+ */
+
+import java.io.*;
+import java.net.*;
+
+
+public class EchoMultiCastDisplay extends Thread {
+
+ 
+  /**
+  *  main method
+  *  accepts a connection, receives a message from client then sends an echo to the client
+  **/
+
+    MulticastSocket multiCastSocket = null;
+
+    EchoMultiCastDisplay(MulticastSocket multicastSocket) {
+
+        this.multiCastSocket = multicastSocket;   
+    }
+
+    public void run(){
+
+      // Build a datagram packet for response
+      byte[] buf = new byte[1000];  
+      DatagramPacket recv = new  DatagramPacket(buf, buf.length);  
+
+      while(true){     
+        try{ 
+          multiCastSocket.receive(recv);
+          String msg = new String(recv.getData(), 0, recv.getLength()); 
+          System.out.println("Response: " + msg);
+
+        } catch (Exception e) {break;}
+      }
+    }
+}
+
+
