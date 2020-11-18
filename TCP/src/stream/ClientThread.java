e64c141a4aeafdafa5062ef18933ae9b5a22f7b8
Phanie Buonomo <fanny.buonomo@insa-lyon.fr>
2020-11-18 10:43:50 +0100

    façon d'écrire le message plus homogène

--- TCP/src/stream/ClientThread.java
+++ TCP/src/stream/ClientThread.java
@@ -44,25 +44,27 @@ extends Thread {
         String pseudo = subpost[1];
         String chatroom = "";
         if(subpost.length == 3) chatroom = subpost[2];
+        
+        String msg = date+" : <"+pseudo+"> ";
         if(currentChat != null){
           switch(tag){
             case "LEAVE":
               EchoServerMultiThreaded.leaveChatroom(this,currentChat);
-              EchoServerMultiThreaded.publishMessage(date+" : "+pseudo+" has left.",currentChat);
+              EchoServerMultiThreaded.publishMessage(msg+"has left.",currentChat);
               currentChat = null;
               break;
             case "CREATE":
               EchoServerMultiThreaded.createChatroom(post);
               currentChat = EchoServerMultiThreaded.joinChatroom(this,chatroom);
-              EchoServerMultiThreaded.publishMessage(date+" : "+pseudo+ "has created the chatroom "+chatroom,currentChat);
+              EchoServerMultiThreaded.publishMessage(msg+ "has created the chatroom "+chatroom,currentChat);
               break;
             case "POST":
-              EchoServerMultiThreaded.publishMessage(date+" : <"+pseudo+">  "+chatroom,currentChat);
+              EchoServerMultiThreaded.publishMessage(msg+chatroom,currentChat);
               break;
             case "JOIN":
               EchoServerMultiThreaded.leaveChatroom(this,currentChat);
               currentChat = EchoServerMultiThreaded.joinChatroom(this,post);
-              EchoServerMultiThreaded.publishMessage(date+" : "+pseudo+" has joined the room.",currentChat);
+              EchoServerMultiThreaded.publishMessage(msg+"has joined the room.",currentChat);
               break;
           }
         }
@@ -72,12 +74,12 @@ extends Thread {
             case "CREATE":
               EchoServerMultiThreaded.createChatroom(post);
               currentChat = EchoServerMultiThreaded.joinChatroom(this,post);
-              EchoServerMultiThreaded.publishMessage(date+" : "+pseudo+ "has created the chatroom "+chatroom,currentChat);
+              EchoServerMultiThreaded.publishMessage(msg+"has created the chatroom "+chatroom,currentChat);
               break;
             case "JOIN":
               EchoServerMultiThreaded.leaveChatroom(this,currentChat);
               currentChat = EchoServerMultiThreaded.joinChatroom(this,post);
-              EchoServerMultiThreaded.publishMessage(date+" : "+pseudo+" has joined the room.",currentChat);
+              EchoServerMultiThreaded.publishMessage(msg+"has joined the room.",currentChat);
               break;
             default:
               socOut.println("You must join a chatroom first");
