28e66e45d99fff7f42708fdecb39f6241b018ca6
Phanie Buonomo <fanny.buonomo@insa-lyon.fr>
2020-11-18 09:40:16 +0100

    ajout des méthodes pour manipuler l'objet chatroom

--- TCP/src/stream/EchoServerMultiThreaded.java
+++ TCP/src/stream/EchoServerMultiThreaded.java
@@ -1,8 +1,8 @@
 /*
- EchoServer
- Example of a TCP server
- Date: 10/01/04
- Authors: BUONOMO Phanie - BATEL Arthur
+   EchoServer
+   Example of a TCP server
+Date: 10/01/04
+Authors: BUONOMO Phanie - BATEL Arthur
  */
 
 
@@ -12,50 +12,77 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EchoServerMultiThreaded  {
-  
- 	/**
-  	* main method
-	* @param EchoServer port
-  	* 
-  	**/
 
-    private static List<ClientThread> connectedThreadList = new ArrayList<>();
+  /**
+   * main method
+   * @param EchoServer port
+   * 
+   **/
 
-    public static void main(String[] args){
-      ServerSocket listenSocket;
-            
-    	if (args.length != 1) {
-            System.out.println("Usage: java EchoServer <EchoServer port>");
-            System.exit(1);
-    	}
-    	try {
-    		listenSocket = new ServerSocket(Integer.parseInt(args[0])); //port
-    		System.out.println("Server ready..."); 
-    		while (true) {
-    			Socket clientSocket = listenSocket.accept();
-    			System.out.println("Connexion from:" + clientSocket.getInetAddress());
-    			ClientThread ct = new ClientThread(clientSocket);
-          addConnectedThread(ct);
-    			ct.start();
-    		}
-      } catch (Exception e) {
-          System.err.println("Error in EchoServer:" + e);
+  private static List<ClientThread> connectedThreadList = new ArrayList<>();
+  private static List<Chatroom> chatroomList = new ArrayList<>();
+
+  public static void main(String[] args){
+    ServerSocket listenSocket;
+
+    if (args.length != 1) {
+      System.out.println("Usage: java EchoServer <EchoServer port>");
+      System.exit(1);
+    }
+    try {
+      listenSocket = new ServerSocket(Integer.parseInt(args[0])); //port
+      System.out.println("Server ready..."); 
+      while (true) {
+        Socket clientSocket = listenSocket.accept();
+        System.out.println("Connexion from:" + clientSocket.getInetAddress());
+        ClientThread ct = new ClientThread(clientSocket);
+        addConnectedThread(ct);
+        ct.start();
       }
+    } catch (Exception e) {
+      System.err.println("Error in EchoServer:" + e);
     }
+  }
 
-    private static void addConnectedThread(ClientThread ct){
-      connectedThreadList.add(ct);
-    }
+  public void createChatroom(String chatName){
+    Chatroom chat = new Chatroom(chatName);
+    chatroomList.add(chat);
+  }
 
-    public static void removeConnectedThread(ClientThread ct){
-      connectedThreadList.remove(ct);
-    }
+  private static void addConnectedThread(ClientThread ct){
+    connectedThreadList.add(ct);
+  }
 
-    public static void publishMessage(String msg){
-      for(ClientThread ct : connectedThreadList){
-        ct.sendMessageToClient(msg);
-      }
+  public static void joinChatroom(ClientThread ct, String nameChat){
+    for(Chatroom chat : chatroomList){
+      if(chat.getName().equals(nameChat))
+        chat.join(ct);
+      break;
     }
+  }
+
+  public static void removeConnectedThread(ClientThread ct){
+    connectedThreadList.remove(ct);
+  }
+
+  public static void leaveChatroom(ClientThread ct, Chatroom chat){
+    chat.leave(ct);
+  }
+
+  public static void publishMessage(String msg){
+    for(ClientThread ct : connectedThreadList){
+      ct.sendMessageToClient(msg);
+    }
+  }
+
+  public static void publishMessage(String msg, Chatroom chat){
+    for(ClientThread ct : chat.getConnectedClients()){
+      ct.sendMessageToClient(msg);
+    }
+    saveMessage(msg,chat);
+  }
+
+  private static void saveMessage(String msg, Chatroom chat){
+    chat.addMessageToHistory(msg);
+  }
 }
-
-  
